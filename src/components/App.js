5408217faa9e04913f1b483d4b0e52d915217a62
Nicolas Henriquez <nhenriquez619@gmail.com>
2021-07-17 02:15:46 -0400

    Add state assistants

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import { uuid } from 'uuidv4'
+import Main from './layouts/Main'
+import AddAssistant from './assistant/AddAssistant'
+import AssistantList from './assistant/AssistantList'
 
 function App() {
+  const LOCAL_STORAGE_KEY = 'assistants'
+
+  // Estado de Ayudantes, almacena y cambia los objetos dentro de ella
+  const [assistants, setAssistants] = useState([])
+
+  // Cambia el estado de los ayudantes
+  const addAssistantHandler = (assistant) => {
+    setAssistants([...assistants, { id: uuid(), ...assistant }])
+  }
+
+  // Elimina un objeto(assistant)
+  const removeAssistantHandler = (id) => {
+    const newAssistantList = assistants.filter((assistant) => {
+      return assistant.id !== id
+    })
+    setAssistants(newAssistantList)
+  }
+
+  // Obtiene los objetos(assistants) guardados en el LocalStorage
+  useEffect(() => {
+    const retriveAssistants = JSON.parse(
+      localStorage.getItem(LOCAL_STORAGE_KEY)
+    )
+    if (retriveAssistants) setAssistants(retriveAssistants)
+  }, [])
+
+  // Guarda el objeto(assistants) en el LocalStorage
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(assistants))
+  }, [assistants])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Main>
+      <AddAssistant addAssistantHandler={addAssistantHandler} />
+      <AssistantList
+        assistant={assistants}
+        getAssistantId={removeAssistantHandler}
+      />
+    </Main>
+  )
 }
 
-export default App;
+export default App
